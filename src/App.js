8a4ab3bbab288439c74caec002ecc0da060b6d00
Antonkosmos-cyba <natalijatsyba@yandex.ru>
2024-09-24 14:45:25 +0300

    fin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Player from "./components/player";
+import PlayerD from "./components/player_2";
 
 function App() {
   return (
     <div className="App">
       <Player />
+      <PlayerD />
     </div>
   );
 }
